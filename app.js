64103925ee38b8fca3f9d35e1722443e9b4814f0
Mbrace1 <michaelbracey24@gmail.com>
2022-08-16 16:28:35 +0100

    Added get user when entering in name

--- app.js
+++ app.js
@@ -7,6 +7,24 @@ app.get('/', async (req, res) => {
     res.send("test")
 })
 
+app.get('/users/:user', async (req, res) => {
+    // two names so Jonah%20West is instead jonah-west
+    // api will only work with - separation
+    const name = req.params.user.toString()
+    const nameSplit = name.split('-')
+    const firstName = nameSplit[0].charAt(0).toUpperCase() + nameSplit[0].slice(1);
+    const lastName = nameSplit[1].charAt(0).toUpperCase() + nameSplit[1].slice(1);
+
+    const combinedName = firstName + " " + lastName
+    console.log(combinedName)
+    const userData = await User.findOne({where: {name: combinedName}})
+
+    if (userData) {
+        res.send(userData)
+    } else {
+        res.send("resource not found")
+    }
+})
 
 const port = 3000
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const {DataTypes, db} = require('../db');
 const User = db.define('user', {
   name: DataTypes.STRING,
   email: DataTypes.STRING,
+  country: DataTypes.STRING,
 });
 
 module.exports = {User};
